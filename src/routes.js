3805d5dc3e1388e54601e390e557b2cde059fab6
Arthur Leonardo <arthurleonardo801@hotmail.com>
2021-08-21 10:20:56 -0300

    Alterada pagina de home para admin

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import HomePage              from "./pages/HomePage"
 import CadastroPage          from "./pages/CadastroPage"
 import LoginPage             from "./pages/loginPage"
 import cadastroUsuarioPage   from "./pages/cadastroUsuarioPage"
+import AdminPage             from "./pages/AdminPage"
 
 const Routes = ()=>(
     <BrowserRouter>
@@ -12,6 +13,7 @@ const Routes = ()=>(
             <Route exact path="/portal/home" component={HomePage} />
             <Route exact path="/portal/cadastro" component={CadastroPage} />
             <Route exact path="/portal/login" component={LoginPage} />
+            <Route exact path="/portal/admin" component={AdminPage} />
             <Route exact path="/portal/cadastroUsuario" component={cadastroUsuarioPage} />
             <Route path="*"> <Redirect to="/portal/login" /> </Route>
         </Switch>
