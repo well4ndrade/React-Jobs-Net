4f98d73924877f63e983b67130f2ab17e9d02e86
Iago Araújo <iagoaraujo498@gmai.com>
2021-08-21 23:43:00 -0300

    Endereço atualização

--- src/components/EnderecoComponent.js
+++ src/components/EnderecoComponent.js
@@ -5,6 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Button from "@material-ui/core/Button";
 import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 
 
@@ -67,22 +68,47 @@ function App() {
             <Paper className={classes.paper}>
                
             <Grid  container  item xs={12}>
-
                  <Grid   item xs={4} spacing={1}> 
-                  <label>Cep</label>
-                  <Field name="cep" type="text" onBlur={(ev) => onBlurCep(ev, setFieldValue)} />
+                  <label>Cep: </label>
+                  <Field name="cep" type="text" onBlur={(ev) => onBlurCep(ev, setFieldValue)} /> 00000-000
                  </Grid>
 
-                 <Grid   item xs={4} spacing={1}> 55555
-                   <label>Logradouro</label>
+                  <Grid  item xs={4} spacing={1}> 
+                   <label>Logradouro: </label>
                    <Field name="logradouro" type="text" />  
                  </Grid>
 
                  <Grid   item xs={4} spacing={1}> 
-                   <label>Número</label>
+                   <label>Número: </label>
                    <Field name="numero" type="text" />
                  </Grid>
-        
+
+                 <Grid   item xs={4} spacing={1}> 
+                   <label>Complemento: </label>
+                   <Field name="complemento" type="text" />
+                 </Grid>
+
+                 <Grid   item xs={4} spacing={1}> 
+                   <label>Bairro: </label>
+                   <Field name="bairro" type="text" />
+                 </Grid>
+                 
+                 <Grid   item xs={4} spacing={1}> 
+                   <label>Cidade: </label>
+                   <Field name="cidade" type="text" />
+                 </Grid>
+
+                  <Grid   item xs={4} spacing={1}> 
+                   <label>Estado: </label>
+                   <Field name="estado" type="text" />
+                 </Grid>
+                 <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth={true}>
+              Concluir Cadastro
+          </Button>
             </Grid>
     
             </Paper>
